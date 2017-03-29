9cabb26a4e9f8204410065beb39f748ef4eb95a3
Duncan Andrew <drma@d-108-179-131-140.dhcp4.washington.edu>
2017-03-29 11:20:50 -0700

    Initial Commit

---
+++ helloios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Duncan Andrew on 3/29/17.
+//  Copyright © 2017 Duncan Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
